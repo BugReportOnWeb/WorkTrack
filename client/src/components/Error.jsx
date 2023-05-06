1b540f7a4de8ad1e7810cee61cee961ae2749ea1
Dev <flashytesting@gmail.com>
2023-05-07 00:57:23 +0530

    Change error styling

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,6 +1,6 @@
 const Error = ({ error }) => {
     return (
-        <div className='absolute p-3 border-2 border-red-500 rounded-md w-50 top-full left-0 mt-20 w-15 -translate-y-1/2'>
+        <div className='absolute p-3 border-2 border-red-500 rounded-md top-[120%] left-0 w-[17.3rem] -translate-y-1/2'>
             <p className='text-red-500'>{error}</p>
         </div>
     )
