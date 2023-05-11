96e95c736abe27fff50b8e035a559e64f49d2ddc
Dev <flashytesting@gmail.com>
2023-05-12 00:14:54 +0530

    Add login functionality && generate JWT token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,22 @@
 import { User } from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
-const loginUser = (_req, res) => {
-    res.json({ message: 'Login user' });
+const createToken = (userId) => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '3d' })
+}
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.login(email, password);
+
+        const token = createToken(user._id);
+
+        res.status(200).json({ email, token });
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 const registerUser = async (req, res) => {
@@ -9,7 +24,10 @@ const registerUser = async (req, res) => {
 
     try {
         const newUser = await User.register(email, password);
-        res.status(200).json(newUser);
+
+        const token = createToken(newUser._id);
+
+        res.status(200).json({ email, token });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
