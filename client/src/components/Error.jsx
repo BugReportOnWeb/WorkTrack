33aac068bf5944f5cb0108dead3d643f0e91d645
Dev <flashytesting@gmail.com>
2023-05-14 21:31:18 +0530

    Add change of width using passed prop

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,6 +1,6 @@
-const Error = ({ error }) => {
+const Error = ({ error, width }) => {
     return (
-        <div className='absolute p-3 border-2 border-red-500 rounded-md top-[120%] left-0 w-[17.3rem] -translate-y-1/2'>
+        <div className={`absolute p-3 border-2 border-red-500 rounded-md top-[120%] left-0 -translate-y-1/2 ${width}`}>
             <p className='text-red-500'>{error}</p>
         </div>
     )
