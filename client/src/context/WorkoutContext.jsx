55c3ad68bdc4ef76911f071f7c4ebc9c1174e8af
Dev <flashytesting@gmail.com>
2023-05-14 21:33:25 +0530

    Update context with a provider component

--- client/src/context/WorkoutContext.jsx
+++ client/src/context/WorkoutContext.jsx
@@ -1,3 +1,18 @@
 import { createContext } from  'react';
+import { useState } from 'react';
 
-export const WorkoutContext = createContext();
+
+const WorkoutContext = createContext();
+
+const WorkoutContextProvider = ({ children }) => {
+    const [workouts, setWorkouts] = useState([]);
+
+    return (
+        <WorkoutContext.Provider value={{ workouts, setWorkouts }}>
+            {children}
+        </WorkoutContext.Provider>
+    )
+}
+
+export { WorkoutContext };
+export default WorkoutContextProvider;
