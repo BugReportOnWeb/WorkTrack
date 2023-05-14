bc5fd2aab18767137d6cb0d67ea2a49f0197ebd5
Dev <flashytesting@gmail.com>
2023-05-14 21:40:20 +0530

    Remove WorkoutContext from Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useContext } from 'react';
 import WorkoutDetails from '../components/WorkoutDetails.jsx';
 import WorkoutForm from '../components/WorkoutForm.jsx';
 import { WorkoutContext } from '../context/WorkoutContext.jsx';
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState([]);
+    const { workouts, setWorkouts } = useContext(WorkoutContext);
 
     useEffect(() => {
         const fetchWorkouts = async () => {
@@ -18,20 +18,18 @@ const Home = () => {
     }, [])
 
     return (
-        <WorkoutContext.Provider value={{ workouts, setWorkouts }}>
-            <div className='Home relative'>
-                <h1 className='font-extrabold text-5xl my-5 mt-10 text-center'>Workouts</h1>
-                {/* {workouts.length === 0 && <h1 className='my-3'>Loading...</h1>} */}
+        <div className='Home relative'>
+            <h1 className='font-extrabold text-5xl my-5 mt-10 text-center'>Workouts</h1>
+            {/* {workouts.length === 0 && <h1 className='my-3'>Loading...</h1>} */}
 
-                <div className='my-14 ml-2 mr-80 grid grid-cols-1 gap-4 md:grid-cols-2'>
-                    {workouts && workouts.map(workout => (
-                        <WorkoutDetails key={workout._id} workout={workout} />
-                    ))}
-                </div>
-                
-                <WorkoutForm />
+            <div className='my-14 ml-2 mr-80 grid grid-cols-1 gap-4 md:grid-cols-2'>
+                {workouts && workouts.map(workout => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
             </div>
-        </WorkoutContext.Provider>
+            
+            <WorkoutForm />
+        </div>
     )
 }
 
