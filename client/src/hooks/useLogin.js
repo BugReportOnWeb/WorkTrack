f3c35688b444bca0087103000e2bb5b6cd3313ef
Dev <flashytesting@gmail.com>
2023-05-14 23:34:01 +0530

    Create useLogin hook

---
+++ client/src/hooks/useLogin.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { useAuthUserContext } from './useAuthUserContext.js';
+
+const useLogin = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const { setAuthUser } = useAuthUserContext();
+
+    const login = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        const res = await fetch('http://localhost:4000/api/user/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        })
+
+        const data = await res.json();
+
+        if (!res.ok) {
+            setIsLoading(false);
+            setError(data.error);
+        }
+
+        if (res.ok) {
+            localStorage.setItem('user', JSON.stringify(data));
+            setIsLoading(false);
+            setAuthUser({ user: data });
+        }
+    }
+
+    return { login, error, isLoading };
+}
+
+export { useLogin };
