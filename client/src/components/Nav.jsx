6821bf3294fd60102ac5171573d9c5a8eaaee281
Dev <flashytesting@gmail.com>
2023-05-15 01:37:49 +0530

    Add check for authUser to display nav links accordingly

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -7,15 +7,24 @@ const Nav = () => {
     const { authUser } = useAuthUserContext();
 
     return (
-        <div className='Nav flex justify-between items-center py-5'>
+        <div className={`Nav flex justify-between items-center ${authUser ? 'py-5' : 'py-7'}`}>
             <Link to='/'>
                 <h1 className='font-bold text-xl'>Work<span className='text-[#007bff]'>Track</span></h1>
             </Link>
             <div className='flex items-center gap-7 font-semibold'>
-                <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/about'>About</Link>
-                <Link className={`underline-offset-8 decoration-2 decoration-[#007bff] hover:underline ${authUser ? 'hidden' : ''}`} to='/login'>Login</Link>
-                <Link className={`underline-offset-8 decoration-2 decoration-[#007bff] hover:underline ${authUser ? 'hidden' : ''}`} to='/register'>SignUp</Link>
-                <button className={`py-2 px-3 border-2 border-[#007bff] rounded-md hover:bg-[#007bff] ${!authUser ? 'hidden' : ''}`} onClick={logout} to='/logout'>Logout</button>
+                {!authUser && (
+                    <>
+                        <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/about'>About</Link>
+                        <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/login'>Login</Link>
+                        <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/register'>SignUp</Link>
+                    </>
+                )}
+                {authUser && (
+                    <>
+                        <p>{authUser.email}</p>
+                        <button className='py-2 px-3 border-2 border-[#007bff] rounded-md hover:bg-[#007bff]' onClick={logout}>Logout</button>
+                    </>
+                )}
             </div>
         </div>
     )
