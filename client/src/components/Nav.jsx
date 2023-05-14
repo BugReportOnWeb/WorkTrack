982fec32f1f012b9686873c8be2aa2800eafe55c
Dev <flashytesting@gmail.com>
2023-05-14 23:58:25 +0530

    Add button for logout && change visibily of other links

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,6 +1,11 @@
 import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout.js';
+import { useAuthUserContext } from '../hooks/useAuthUserContext.js';
 
 const Nav = () => {
+    const { logout } = useLogout();
+    const { authUser } = useAuthUserContext();
+
     return (
         <div className='Nav flex justify-between items-center py-5'>
             <Link to='/'>
@@ -8,8 +13,9 @@ const Nav = () => {
             </Link>
             <div className='flex items-center gap-7 font-semibold'>
                 <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/about'>About</Link>
-                <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/login'>Login</Link>
-                <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/register'>SignUp</Link>
+                <Link className={`underline-offset-8 decoration-2 decoration-[#007bff] hover:underline ${authUser ? 'hidden' : ''}`} to='/login'>Login</Link>
+                <Link className={`underline-offset-8 decoration-2 decoration-[#007bff] hover:underline ${authUser ? 'hidden' : ''}`} to='/register'>SignUp</Link>
+                <button className={`py-2 px-3 border-2 border-[#007bff] rounded-md hover:bg-[#007bff] ${!authUser ? 'hidden' : ''}`} onClick={logout} to='/logout'>Logout</button>
             </div>
         </div>
     )
