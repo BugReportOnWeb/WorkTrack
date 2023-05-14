283fc1fcc141f019aa85deb79b368bc586e4a180
Dev <flashytesting@gmail.com>
2023-05-14 21:31:41 +0530

    Add link for about, login and register

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        <div className='Nav flex justify-between items-center my-5'>
+        <div className='Nav flex justify-between items-center py-5'>
             <Link to='/'>
                 <h1 className='font-bold text-xl'>Work<span className='text-[#007bff]'>Track</span></h1>
             </Link>
             <div className='flex items-center gap-7 font-semibold'>
-                <Link className='underline-offset-4 decoration-2 decoration-[#007bff] hover:underline' to='/about'>About</Link>
-                <Link className='underline-offset-4 decoration-2 decoration-[#007bff] hover:underline' to='/login'>Login</Link>
-                <Link className='underline-offset-4 decoration-2 decoration-[#007bff] hover:underline' to='/sign-up'>SignUp</Link>
+                <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/about'>About</Link>
+                <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/login'>Login</Link>
+                <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/register'>SignUp</Link>
             </div>
         </div>
     )
