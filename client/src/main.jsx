a62f205aa34198a281ed51cc1e8137fcbfa25aa2
Dev <flashytesting@gmail.com>
2023-05-16 22:40:57 +0530

    Add AuthUserContextProvider Context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import './index.css'
+import AuthUserContextProvider from './context/AuthUserContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <AuthUserContextProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </AuthUserContextProvider>
   </React.StrictMode>,
 )
