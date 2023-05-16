9ad98a9013fee61fbdc9e7db9c798ff47741ee68
Dev <flashytesting@gmail.com>
2023-05-17 01:10:11 +0530

    Add userId field to the schema

--- server/models/workoutModel.js
+++ server/models/workoutModel.js
@@ -14,6 +14,10 @@ const workoutSchema = new Schema({
     reps: {
         type: Number,
         required: true,
+    },
+    userId: {
+        type: String,
+        required: true
     }
 }, { timestamps: true })
 
