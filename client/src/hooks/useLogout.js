9d8b0f91cfd635c336f307801c64941e3788c0fd
Dev <flashytesting@gmail.com>
2023-05-17 01:09:15 +0530

    Reset workout gloabal state to empty array after logout

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
 import { useAuthUserContext } from '../hooks/useAuthUserContext.js';
+import { WorkoutContext } from '../context/WorkoutContext.jsx';
 
 const useLogout = () => {
     const { setAuthUser } = useAuthUserContext();
+    const { setWorkouts } = useContext(WorkoutContext);
 
     const logout = () => {
         localStorage.removeItem('user');
         setAuthUser(null);
+        setWorkouts([]);
     }
 
     return { logout }
