c9423400071b4956a1ebff91d958324b940ee366
Dev <flashytesting@gmail.com>
2023-05-16 22:34:56 +0530

    Move AuthUserContextProvider to parent (main.jsx) element

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 // Components
 import Nav from './components/Nav.jsx';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
-import Contact from './pages/Contact.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
-
 import WorkoutContextProvider from './context/WorkoutContext.jsx';
-import AuthUserContextProvider from './context/AuthUserContext.jsx';
+import { useAuthUserContext } from './hooks/useAuthUserContext.js';
 
 const App = () => {
+    const { authUser } = useAuthUserContext();
+
     return (
-        <AuthUserContextProvider>
-            <WorkoutContextProvider>
-                <div className='App max-w-5xl mx-auto px-5'>
-                    <Nav />
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/about' element={<About />} />
-                        <Route path='/contact' element={<Contact />} />
-                        <Route path='/login' element={<Login />} />
-                        <Route path='/register' element={<Register />} />
-                    </Routes>
-                </div>
-            </WorkoutContextProvider>
-        </AuthUserContextProvider>
+        <WorkoutContextProvider>
+            <div className='App max-w-5xl mx-auto px-5'>
+                <Nav />
+                <Routes>
+                    <Route path='/' element={authUser ? <Home /> : <Navigate to='/login' />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/login' element={!authUser ? <Login /> : <Navigate to='/' />} />
+                    <Route path='/register' element={!authUser ? <Register /> : <Navigate to='/' />} />
+                </Routes>
+            </div>
+        </WorkoutContextProvider>
     )
 }
 
