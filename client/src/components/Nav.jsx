fad2f39755ad8fd683d74a0a3f5dc0ea293e2b2d
Dev <flashytesting@gmail.com>
2023-05-16 17:15:37 +0530

    Update UI of logout button link

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -22,7 +22,12 @@ const Nav = () => {
                 {authUser && (
                     <>
                         <p>{authUser.email}</p>
-                        <button className='py-2 px-3 border-2 border-[#007bff] rounded-md hover:bg-[#007bff]' onClick={logout}>Logout</button>
+                        {/* <button className='py-2 px-3 border-2 border-[#007bff] rounded-md hover:bg-[#007bff]' onClick={logout}>Logout</button> */}
+
+                        <button className="relative py-2 px-3 rounded-md overflow-hidden bg-blue-500 border-2 border-blue-500 text-white transition duration-300 ease-in-out hover:bg-blue-600 hover:border-blue-600" onClick={logout}>
+                          <span className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-r from-blue-600 to-blue-700 opacity-0 transition duration-300 ease-in-out"></span>
+                            Logout
+                        </button>
                     </>
                 )}
             </div>
