1d5536b26d91798f73d501e17641a63363ac1278
Dev <flashytesting@gmail.com>
2023-05-16 21:42:13 +0530

    Add middleware for protecting API from unauthorized access

---
+++ server/middleware/requireAuth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../models/userModel.js';
+
+const requireAuth = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) return res.status(401).json({ error: 'Authorization token required' });
+
+    const token = authorization.split(' ')[1];
+
+    try {
+        const { _id } = jwt.verify(token, process.env.JWT_SECRET);
+        res.user = User.findById(_id).select('_id');
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Request is not authorized' });
+    }
+}
+
+export { requireAuth };
