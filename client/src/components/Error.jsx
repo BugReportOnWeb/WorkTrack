ff78c71bbe13362a22be87117b1744e69501a5cc
Dev <flashytesting@gmail.com>
2023-05-19 00:04:00 +0530

    Update error box top position

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,6 +1,6 @@
 const Error = ({ error, width }) => {
     return (
-        <div className={`absolute p-3 border-2 border-red-500 rounded-md top-[120%] left-0 -translate-y-1/2 ${width}`}>
+        <div className={`absolute left-0 p-3 border-2 border-red-500 rounded-md top-[116%] -translate-y-1/2 ${width}`}>
             <p className='text-red-500'>{error}</p>
         </div>
     )
