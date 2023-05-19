682200a0675e25979d7cf5e2674383255bac5b19
Dev <flashytesting@gmail.com>
2023-05-20 00:49:24 +0530

    Add responsiveness to navbar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,31 +1,43 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout.js';
 import { useAuthUserContext } from '../hooks/useAuthUserContext.js';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { IoMdClose } from 'react-icons/io';
 
 const Nav = () => {
     const { logout } = useLogout();
     const { authUser } = useAuthUserContext();
+    const [showNavLinks, setShowNavLinks] = useState(false)
+
+    const toggleNavLinks = () => {
+        setShowNavLinks(prevState => {
+            return !prevState
+        })
+    }
 
     return (
-        <div className={`Nav flex justify-between items-center ${authUser ? 'py-5' : 'py-7'}`}>
+        <div className={`relative z-10 flex justify-between items-center ${authUser ? 'py-5' : 'py-7'}`}>
             <Link to='/'>
                 <h1 className='font-bold text-xl'>Work<span className='text-[#007bff]'>Track</span></h1>
             </Link>
-            <div className='flex items-center gap-7 font-semibold'>
+
+            {!showNavLinks && <RxHamburgerMenu className='text-2xl cursor-pointer sm:hidden' onClick={toggleNavLinks} />}
+            {showNavLinks && <IoMdClose className='text-2xl cursor-pointer sm:hidden' onClick={toggleNavLinks} />}
+
+            <div className={`absolute bg-gray-800 top-[80%] right-1 flex flex-col p-5 gap-3 rounded-md border-2 border-gray-800 ${!showNavLinks ? 'hidden' : ''} sm:flex sm:flex-row sm:items-center sm:static sm:bg-transparent sm:gap-7 sm:border-none sm:p-0`}>
                 {!authUser && (
                     <>
-                        <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/about'>About</Link>
-                        <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/login'>Login</Link>
-                        <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/register'>SignUp</Link>
+                        <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/about' onClick={toggleNavLinks}>About</Link>
+                        <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/login' onClick={toggleNavLinks}>Login</Link>
+                        <Link className='underline-offset-8 decoration-2 decoration-[#007bff] hover:underline' to='/register' onClick={toggleNavLinks}>SignUp</Link>
                     </>
                 )}
                 {authUser && (
                     <>
-                        <p>{authUser.email}</p>
-                        {/* <button className='py-2 px-3 border-2 border-[#007bff] rounded-md hover:bg-[#007bff]' onClick={logout}>Logout</button> */}
-
+                        <p>{authUser.email}</p> 
                         <button className="relative py-2 px-3 rounded-md overflow-hidden bg-blue-500 border-2 border-blue-500 text-white transition duration-300 ease-in-out hover:bg-blue-600 hover:border-blue-600" onClick={logout}>
-                          <span className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-r from-blue-600 to-blue-700 opacity-0 transition duration-300 ease-in-out"></span>
+                          <span className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-r from-blue-600 to-blue-700 opacity-0 transition duration-300 ease-in-out" onClick={toggleNavLinks}></span>
                             Logout
                         </button>
                     </>
