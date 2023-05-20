602b4fbea9ecead8cbea4ee97cf29a374dd1d55f
Dev <flashytesting@gmail.com>
2023-05-20 23:35:34 +0530

    Add on loading element

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,7 @@
+const Loading = () => {
+    return (
+        <div className='inline-block mr-2 w-5 text-center h-5 mx-auto rounded-full border-2 border-gray-700 border-s-gray-400 animate-spin'></div>
+    )
+}
+
+export default Loading;
