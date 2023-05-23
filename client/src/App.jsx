622ae60cbecb3bf925ff7e3a38a46a9634093b15
Dev <flashytesting@gmail.com>
2023-05-23 23:25:37 +0530

    Wrap pages inside AnimatePresence

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,20 +8,25 @@ import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import WorkoutContextProvider from './context/WorkoutContext.jsx';
 import { useAuthUserContext } from './hooks/useAuthUserContext.js';
+import { useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 const App = () => {
     const { authUser } = useAuthUserContext();
+    const location = useLocation();
 
     return (
         <WorkoutContextProvider>
             <div className='App max-w-5xl mx-auto px-5'>
                 <Nav />
-                <Routes>
-                    <Route path='/' element={authUser ? <Home /> : <Navigate to='/login' />} />
-                    <Route path='/about' element={<About />} />
-                    <Route path='/login' element={!authUser ? <Login /> : <Navigate to='/' />} />
-                    <Route path='/register' element={!authUser ? <Register /> : <Navigate to='/' />} />
-                </Routes>
+                <AnimatePresence initial={false}>
+                    <Routes key={location.pathname}>
+                        <Route path='/' element={authUser ? <Home /> : <Navigate to='/login' />} />
+                        <Route path='/about' element={<About />} />
+                        <Route path='/login' element={!authUser ? <Login /> : <Navigate to='/' />} />
+                        <Route path='/register' element={!authUser ? <Register /> : <Navigate to='/' />} />
+                    </Routes>
+                </AnimatePresence>
             </div>
         </WorkoutContextProvider>
     )
