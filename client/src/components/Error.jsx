489631beb934b1cbe672eb666b743375781db714
Dev <flashytesting@gmail.com>
2023-05-24 19:11:21 +0530

    Fix top margin on dynamic form page layout

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,6 +1,6 @@
 const Error = ({ error, width }) => {
     return (
-        <div className={`absolute w-full left-0 p-3 border-2 border-red-500 rounded-md top-[116%] -translate-y-1/2 ${width}`}>
+        <div className={`absolute -z-10 w-full left-0 p-3 border-2 border-red-500 rounded-md top-full mt-12 -translate-y-1/2 ${width}`}>
             <p className='text-red-500'>{error}</p>
         </div>
     )
