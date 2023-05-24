1942afcbde9628d929d28e41036ec6d57b022a3c
Dev <flashytesting@gmail.com>
2023-05-24 19:17:18 +0530

    Change border width

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -23,12 +23,12 @@ const Login = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.75, ease: 'easeOut' }}
-            className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-zinc-800 pt-8 pb-7 px-8 flex flex-col justify-center items-center gap-5 w-fit border-2 border-zinc-700 rounded-lg' onSubmit={loginUser}
+            className='absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-zinc-800 pt-8 pb-7 px-8 flex flex-col justify-center items-center gap-5 w-fit border border-zinc-700 rounded-lg' onSubmit={loginUser}
         >
             <h1 className='text-3xl font-bold'>Login</h1>
 
             <input
-                className='p-2 bg-zinc-700 border-2 border-zinc-600 rounded-md outline-none'
+                className='p-2 bg-zinc-700 border border-zinc-600 rounded-md outline-none'
                 type='email'
                 onChange={e => setEmail(e.target.value)}
                 value={email}
@@ -36,14 +36,14 @@ const Login = () => {
             />
 
             <input
-                className='p-2 bg-zinc-700 border-2 border-zinc-600 rounded-md outline-none'
+                className='p-2 bg-zinc-700 border border-zinc-600 rounded-md outline-none'
                 type='password'
                 onChange={e => setPassword(e.target.value)}
                 value={password}
                 placeholder='Password'
             />
 
-            <button type='submit' disabled={isLoading} className='flex justify-center items-center min-w-[5rem] border-2 border-[#007bff] p-3 rounded-lg outline-none hover:bg-[#007bff] hover:text-white'>
+            <button type='submit' disabled={isLoading} className='flex justify-center items-center min-w-[5rem] border border-[#007bff] p-3 rounded-lg outline-none hover:bg-[#007bff] hover:text-white'>
                 {isLoading && <Loading />}
                 Login
             </button>
